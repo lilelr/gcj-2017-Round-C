5f6f9d420f6a16fd5cf654c68879029c92bdad3f
lilelr <lilelr@163.com>
2016-09-18 12:15:15 +0800

    Initial Commit

---
+++ gcj 2017 Round C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gcj 2017 Round C
+//
+//  Created by YuXiao on 9/18/16.
+//  Copyright © 2016 YuXiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
